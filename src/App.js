ebab42c4c9c5b94c4d24c3de32d317250b4cfffb
Okunade sherif <o.d.alsherif@gmail.com>
2022-09-17 20:00:25 +0100

    Routing all pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './css/style';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Message from "./Pages/Message";
+//import Navbar from "./components/Navbar";
+//import Footer from "./components/Footer";
+import Activity from "./Pages/Activity";
+import Settings from "./Pages/Settings";
+
 
 function App() {
   return (
     <div className="App">
-       <h1> hello sass</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Message" element={<Message />} />
+          <Route path="/Activity" element={<Activity />} />
+          <Route path="/Settings" element={<Settings />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
