578d5e344de9d2937e2f79a90d20dd85028ff875
Imam Rasheedat <imamrasheedatahmad1993@gmail.com>
2022-09-19 10:32:48 +0100

    i am done with the search component and the story

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import Story from '../Components/Story';
 function Home() {
   return (
     <div className='home'>
-      <h1>Home</h1>
+      <div className="home-search">
+        <div className="searchicons"><input type="text" className='search' placeholder='search'/><i className=' fa fa-search'></i></div>
+        <h3>Hey,Rash</h3>
+      </div>
+      <div className='home-trend'>
+        <h2>Trending Posts Today</h2>
+        <div className='storypictures'>
+          <Story/>
+          <div className="storybutton"><button><i className='fa-solid fa-plus'></i></button></div>
+        </div>
+      </div>
+      <div className='home-post'>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
